d9d69f4c0c34b7c3673b123e0f55dcec68d3a568
victo <victo@VICSLAPTOP>
2025-02-06 01:00:50 -0600

    CRUD completo

--- Controllers/equiposController.cs
+++ Controllers/equiposController.cs
@@ -17,6 +17,8 @@ namespace Practica1.Controllers
             _equiposContext = equiposContext;
         }
 
+        // Para poder ver todos los registros:
+
         [HttpGet]
         [Route("GetAll")]
 
@@ -29,6 +31,101 @@ namespace Practica1.Controllers
                 return NotFound();
             }
             return Ok(listadoEquipo);
+        }
+
+        // Para buscar los registros por ID
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+
+        public IActionResult Get(int id)
+        {
+            equipos? equipo = (from e in _equiposContext.equipos where e.id_equipos == id select e).FirstOrDefault();
+
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return Ok(equipo);
+        }
+
+        // Para buscar por texto (Campo descipcion)
+
+        [HttpGet]
+        [Route("Find/{filtro}")]
+
+        public IActionResult FindByDescription(string filtro)
+        {
+            equipos? equipo = (from e in _equiposContext.equipos where e.descripcion.Contains(filtro) select e).FirstOrDefault();
+
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return Ok(equipo);
+        }
+
+        // Para agregar un nuevo registro:
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarEquipo([FromBody] equipos equipo)
+        {
+            try
+            {
+                _equiposContext.equipos.Add(equipo);
+                _equiposContext.SaveChanges();
+                return Ok(equipo);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // Para actualizar un registro
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar)
+        {
+            equipos? equipoActual = (from e in _equiposContext.equipos where e.id_equipos == id select e).FirstOrDefault();
+
+            if (equipoActual == null) 
+            {return NotFound();}
+
+            equipoActual.nombre = equipoModificar.nombre;
+            equipoActual.descripcion = equipoModificar.descripcion;
+            equipoActual.marca_id = equipoModificar.marca_id;
+            equipoActual.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+            equipoActual.anio_compra = equipoModificar.anio_compra;
+            equipoActual.costo = equipoModificar.costo;
+
+            _equiposContext.Entry(equipoActual).State = EntityState.Modified;
+            _equiposContext.SaveChanges();
+
+            return Ok(equipoModificar);
+        }
+
+        // Para Eliminar un registro
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+
+        public IActionResult EliminarEquipo(int id)
+        {
+            equipos? equipo = (from e in _equiposContext.equipos where e.id_equipos == id select e).FirstOrDefault();
+
+            if (equipo == null)
+            { return NotFound(); }
+
+            _equiposContext.equipos.Attach(equipo);
+            _equiposContext.equipos.Remove(equipo);
+            _equiposContext.SaveChanges();
+
+            return Ok(equipo);
         }
     }
 }
